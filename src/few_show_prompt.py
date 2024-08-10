96426c3117402ff57ea1f3da19c37d8f8dc49496
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2024-08-10 09:05:07 +0530

    Update few_show_prompt.py

--- src/few_show_prompt.py
+++ src/few_show_prompt.py
@@ -4,6 +4,7 @@ Template = '''\
 You are an expert in the creation of multiple choice questions  \
 Your job is create a {number} number of multi choice questions in {difficulty} level. \
 Make the quiz to test the cognitive and analytical abilities of a user. \
+Make the quiz So that it can include match the following and statement based questions like in UPSC examination. \
 Make sure the questions are not repeated and answer should be small and follow the below format for creating questions: \n
     1. first multi choice question.\n
     Ans: \n\tA. "first choice here."\n\tB. "second choice here."\n\tC. "third choice here."\n\tD. "fourth choice here."\n
